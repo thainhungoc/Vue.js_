1adadb87692b17c6d9f38a909d4e8c954ca88b0a
Yuamy205 <ngocarmy0710g@gmail.com>
2024-10-17 11:27:32 +0700

    ss4-5

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 // import Ex1 from "./components/Ex1.vue"
 // import Ex2 from "./components/Ex2.vue"
-import Ex3 from "./components/Ex3.vue"
+// import Ex3 from "./components/Ex3.vue"
+import Bai1 from "./components/Bai1.vue"
 </script>
 
 <template>
  <Ex1></Ex1> 
  <Ex2></Ex2>
  <Ex3></Ex3>
+ <Bai1></Bai1>
 </template>
 
 <style scoped lang="scss">
---
+++ src/components/Bai1.vue
@@ -0,0 +1,23 @@
+<script setup lang ="ts">
+import {ref, watch} from "vue"
+const count = ref(0)
+ const handleclick = ()=>{
+    count.value++
+ };
+watch(count, (newValue, oldValue) => {
+    alert(`Giá trị của count trước khi bấm: ${oldValue}, Giá trị sau khi đổi của count: ${newValue}`);
+});
+
+
+</script>
+
+
+<template>
+  <button @click="handleclick">Click</button>
+</template>
+
+
+
+<style>
+
+</style>
